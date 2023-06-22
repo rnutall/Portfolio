3cc2b5c96ad92a2639a8e6c2bdb3c420c030742b
rnutall <rnutall1@gmail.com>
2023-06-22 03:53:50 -0500

    added to home index.js

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -14,15 +14,18 @@ export default function Home() {
         getting a chance to perfect them with a group of brilliant individuals.
         It would be an exciting experience to learn from a world class team of
         developers, Willing to mentor an entrepreneur who is a novice at web
-        dev. but no stranger to business, building from the ground up and using
-        the necessary tools. So please enjoy my Portfolio this is an exciting
-        and thing for me as a life learner; to go include this in my resume of
-        learned things.
+        dev. but no stranger to customer service, hard work, late nights, early
+        morning deadlines, business, building from the ground up, and using the
+        necessary tools in us to get the job done. Please enjoy my Portfolio
+        this is an exciting journey for me; as a Master Barber/ entrepreneur of
+        20yrs; too learning Web Development and advanced concepts. For me, as an
+        entrepreneur and life learner; to be able to include these in my resume
+        of learned things, is amazing. Thank you again for your time in looking
+        over my Portfolio. If one should have any Questions, Please feel free
+        too shoot me an email. Thanks again for checking out my work. I looK
+        forward to working with you. 
         
-         Questions, feel free too shoot me an email. Thanks again
-        for checking out my work. I looK forward to working with you.
-        
-         Rodney Nutall
+         Nutall
       </p>
     </div>
   );
